dd9b04e73693540afa37c0196be38b522da6d65a
NC <n.chimarev@student.helmo.be>
2024-11-20 13:56:44 +0100

    first commit and push

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+# URL de l'API
+url = "https://official-joke-api.appspot.com/random_joke"
+
+# Requête GET à l'API
+response = requests.get(url)
+
+# Vérification du statut de la réponse
+if response.status_code == 200:
+    # Décodage de la réponse JSON
+    joke = response.json()
+    print(f"Voici une blague :\n{joke['setup']}\n{joke['punchline']}")
+else:
+    print(f"Erreur lors de la récupération de la blague. Statut : {response.status_code}")
