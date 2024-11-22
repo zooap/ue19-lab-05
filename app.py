762fd97c2999c71543cb9b134653d00207440c62
NC <n.chimarev@student.helmo.be>
2024-11-22 14:40:21 +0100

    test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 
+print('Hello')
 # URL de l'API
 url = "https://official-joke-api.appspot.com/random_joke"
 
@@ -11,6 +12,5 @@ if response.status_code == 200:
     # Décodage de la réponse JSON
     joke = response.json()
     print(f"Voici une blague :\n{joke['setup']}\n{joke['punchline']}")
-    print('test')
 else:
     print(f"Erreur lors de la récupération de la blague. Statut : {response.status_code}")
