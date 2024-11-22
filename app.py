4f480dc551678b53d6885c59924632538b735041
NC <n.chimarev@student.helmo.be>
2024-11-22 15:01:29 +0100

    test docker

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ response = requests.get(url)
 if response.status_code == 200:
     # Décodage de la réponse JSON
     joke = response.json()
-    print(f"Voici une blague :\n{joke['setup']}\n{joke['punchline']}")
+    print(f"Voici une blague OwO :\n{joke['setup']}\n{joke['punchline']}")
 else:
     print(f"Erreur lors de la récupération de la blague. Statut : {response.status_code}")
