55ad5d229ac7a86e2aa903c85dcae4b9d8198656
NC <n.chimarev@student.helmo.be>
2024-11-22 14:29:09 +0100

    test

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ if response.status_code == 200:
     # Décodage de la réponse JSON
     joke = response.json()
     print(f"Voici une blague :\n{joke['setup']}\n{joke['punchline']}")
+    print('test')
 else:
     print(f"Erreur lors de la récupération de la blague. Statut : {response.status_code}")
